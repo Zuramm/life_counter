4df9f6bfa65a809697fe174821cca2d76642395b
Zuramm <operation@buwogqm.awsapps.com>
2024-04-04 13:31:37 +0200

    fix: add safe margin to main view

--- lib/views/settings.dart
+++ lib/views/settings.dart
@@ -14,128 +14,131 @@ class _SettingsState extends State<Settings> {
   int players = 2;
   int startingLife = 20;
 
-  TextEditingController _textFieldController = TextEditingController();
+  final TextEditingController _textFieldController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              "Life Counter",
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              "Number of Players",
-              style: Theme.of(context).textTheme.bodyLarge,
-            ),
-            SegmentedButton<int>(
-              multiSelectionEnabled: false,
-              emptySelectionAllowed: false,
-              showSelectedIcon: false,
-              selected: {players},
-              onSelectionChanged: (Set<int> newSelection) {
-                setState(() {
-                  players = newSelection.first;
-                });
-              },
-              segments: [1, 2, 3, 4, 5, 6]
-                  .map<ButtonSegment<int>>(
-                    (int value) => ButtonSegment<int>(
-                        value: value, label: Text(value.toString())),
-                  )
-                  .toList(),
-            ),
-            SizedBox(height: 8.0),
-            Text(
-              "Starting Life",
-              style: Theme.of(context).textTheme.bodyLarge,
-            ),
-            SegmentedButton<int>(
-              multiSelectionEnabled: false,
-              emptySelectionAllowed: false,
-              showSelectedIcon: false,
-              selected: {startingLife},
-              onSelectionChanged: (Set<int> newSelection) async {
-                int? newStartingLife;
-
-                if (newSelection.first == 0) {
-                  newStartingLife = await showDialog<int?>(
-                    context: context,
-                    builder: (context) {
-                      _textFieldController.text = startingLife.toString();
-                      return AlertDialog(
-                        title: Text('Set Custom Starting Life'),
-                        content: TextField(
-                          controller: _textFieldController,
-                          decoration:
-                              InputDecoration(hintText: "Starting Life"),
-                          keyboardType: TextInputType.number,
-                          inputFormatters: <TextInputFormatter>[
-                            FilteringTextInputFormatter.digitsOnly
-                          ],
-                        ),
-                        actions: <Widget>[
-                          TextButton(
-                            child: Text('Cancel'),
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                          ),
-                          ElevatedButton(
-                            child: Text('Set'),
-                            onPressed: () {
-                              Navigator.pop(
-                                context,
-                                int.tryParse(
-                                  _textFieldController.text,
-                                  radix: 10,
-                                ),
-                              );
-                            },
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                } else {
-                  newStartingLife = newSelection.first;
-                }
-
-                if (newStartingLife != null) {
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                "Life Counter",
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              const SizedBox(height: 16.0),
+              Text(
+                "Number of Players",
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+              SegmentedButton<int>(
+                multiSelectionEnabled: false,
+                emptySelectionAllowed: false,
+                showSelectedIcon: false,
+                selected: {players},
+                onSelectionChanged: (Set<int> newSelection) {
                   setState(() {
-                    startingLife = newStartingLife!;
+                    players = newSelection.first;
                   });
-                }
-              },
-              segments: [10, 20, 30, 40, 50, 0]
-                  .map<ButtonSegment<int>>(
-                    (int value) => ButtonSegment<int>(
-                        value: value,
-                        label: Text(value == 0 ? "Custom" : value.toString())),
-                  )
-                  .toList(),
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              child: Text("Start"),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => Counter(
-                      players: players,
-                      startingLife: startingLife,
+                },
+                segments: [1, 2, 3, 4, 5, 6]
+                    .map<ButtonSegment<int>>(
+                      (int value) => ButtonSegment<int>(
+                          value: value, label: Text(value.toString())),
+                    )
+                    .toList(),
+              ),
+              const SizedBox(height: 8.0),
+              Text(
+                "Starting Life",
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+              SegmentedButton<int>(
+                multiSelectionEnabled: false,
+                emptySelectionAllowed: false,
+                showSelectedIcon: false,
+                selected: {startingLife},
+                onSelectionChanged: (Set<int> newSelection) async {
+                  int? newStartingLife;
+
+                  if (newSelection.first == 0) {
+                    newStartingLife = await showDialog<int?>(
+                      context: context,
+                      builder: (context) {
+                        _textFieldController.text = startingLife.toString();
+                        return AlertDialog(
+                          title: const Text('Set Custom Starting Life'),
+                          content: TextField(
+                            controller: _textFieldController,
+                            decoration: const InputDecoration(
+                                hintText: "Starting Life"),
+                            keyboardType: TextInputType.number,
+                            inputFormatters: <TextInputFormatter>[
+                              FilteringTextInputFormatter.digitsOnly
+                            ],
+                          ),
+                          actions: <Widget>[
+                            TextButton(
+                              child: const Text('Cancel'),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                            ElevatedButton(
+                              child: const Text('Set'),
+                              onPressed: () {
+                                Navigator.pop(
+                                  context,
+                                  int.tryParse(
+                                    _textFieldController.text,
+                                    radix: 10,
+                                  ),
+                                );
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  } else {
+                    newStartingLife = newSelection.first;
+                  }
+
+                  if (newStartingLife != null) {
+                    setState(() {
+                      startingLife = newStartingLife!;
+                    });
+                  }
+                },
+                segments: [10, 20, 30, 40, 50, 0]
+                    .map<ButtonSegment<int>>(
+                      (int value) => ButtonSegment<int>(
+                          value: value,
+                          label:
+                              Text(value == 0 ? "Custom" : value.toString())),
+                    )
+                    .toList(),
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                child: const Text("Start"),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Counter(
+                        players: players,
+                        startingLife: startingLife,
+                      ),
                     ),
-                  ),
-                );
-              },
-            ),
-          ],
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
